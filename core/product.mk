920b040c7fbba270812b2013551f33bfa2fa4d00
Michael Vines <mvines@codeaurora.org>
2014-11-03 15:36:29 -0800

    Make |lunch| faster by trimming the directories searched for product files.
    Change-Id: I8f67eebcfb96ee17c4d6b2f6da896423c611221c

--- core/product.mk
+++ core/product.mk
@@ -28,8 +28,7 @@
 # $(call ) isn't necessary.
 #
 define _find-android-products-files
-$(shell test -d device && find device -maxdepth 6 -name AndroidProducts.mk) \
-  $(shell test -d vendor && find vendor -maxdepth 6 -name AndroidProducts.mk) \
+$(shell test -d device && find device -maxdepth 3 -name AndroidProducts.mk) \
   $(SRC_TARGET_DIR)/product/AndroidProducts.mk
 endef
 
