91b80dcc60539c70fd22a20a4de00d69c11e38ea
Seinlin <seinlin.maung@gmail.com>
2020-05-14 14:00:40 -0700

    Bug 89807 - Update change to x86.

--- target/product/aosp_x86.mk
+++ target/product/aosp_x86.mk
@@ -30,10 +30,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_common.mk)
 $(call inherit-product-if-exists, device/generic/goldfish/x86-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulator_vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/board/generic_x86/device.mk)
+$(call inherit-product, gonk-misc/b2g.mk)
 
 # Enable mainline checking for excat this product name
 ifeq (aosp_x86,$(TARGET_PRODUCT))
-PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
+#PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS := relaxed
 endif
 
 PRODUCT_NAME := aosp_x86
